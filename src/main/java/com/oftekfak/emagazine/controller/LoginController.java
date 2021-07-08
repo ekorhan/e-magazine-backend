9eb6be1f1b367aad36a5bf539946cff50a89ea5f
Enes Korhan <enes.korhan@etiya.com>
2021-07-08 18:15:42 +0300

    user Id has been set after login and register

--- src/main/java/com/oftekfak/emagazine/controller/LoginController.java
+++ src/main/java/com/oftekfak/emagazine/controller/LoginController.java
@@ -4,6 +4,7 @@ import com.oftekfak.emagazine.model.registration.LoginRequest;
 import com.oftekfak.emagazine.model.registration.LoginService;
 import com.oftekfak.emagazine.model.registration.TokenModel;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,6 +17,10 @@ public class LoginController {
 
     @PostMapping(path = "/login")
     public ResponseEntity<TokenModel> login(@RequestBody LoginRequest request) {
-        return ResponseEntity.ok(loginService.login(request));
+        try {
+            return ResponseEntity.ok(loginService.login(request));
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null);
+        }
     }
 }
