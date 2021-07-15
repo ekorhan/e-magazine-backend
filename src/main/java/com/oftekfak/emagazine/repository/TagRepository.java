1f9821fe492e45683ac9585d01d7cf4a304ef8b3
Enes Korhan <enes.korhan@etiya.com>
2021-07-15 20:31:05 +0300

    Tag fix.

--- src/main/java/com/oftekfak/emagazine/repository/TagRepository.java
+++ src/main/java/com/oftekfak/emagazine/repository/TagRepository.java
@@ -4,9 +4,11 @@ import com.oftekfak.emagazine.entity.TagEntity;
 import com.oftekfak.emagazine.model.post.TagModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface TagRepository extends JpaRepository<TagEntity, Long> {
     @Query("select new com.oftekfak.emagazine.model.post.TagModel(t) from TagEntity t where t.active = true")
     List<TagModel> getAllTags();
--- src/main/java/com/oftekfak/emagazine/service/impl/UtilityServiceImpl.java
+++ src/main/java/com/oftekfak/emagazine/service/impl/UtilityServiceImpl.java
@@ -3,13 +3,15 @@ package com.oftekfak.emagazine.service.impl;
 import com.oftekfak.emagazine.model.post.TagModel;
 import com.oftekfak.emagazine.repository.TagRepository;
 import com.oftekfak.emagazine.service.IUtilityService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class UtilityServiceImpl implements IUtilityService {
-    TagRepository tagRepository;
+    @Autowired
+    private TagRepository tagRepository;
 
     @Override
     public List<TagModel> inquireAllTags() {
